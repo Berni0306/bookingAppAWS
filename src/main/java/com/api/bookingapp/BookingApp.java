6e21be82e6496f4cf08fa986f986dc8ea9d5a7af
Berni0306 <114695861+Berni0306@users.noreply.github.com>
2024-10-09 14:40:01 -0600

    Removed sensitive data

--- src/main/java/com/api/bookingapp/BookingApp.java
+++ src/main/java/com/api/bookingapp/BookingApp.java
@@ -1,11 +1,17 @@
 package com.api.bookingapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
 @EnableScheduling
 public class BookingApp {
+	// silence console logging
+	@Value("${logging.level.root:OFF}")
+	String message = "";
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApp.class, args);
 	}
